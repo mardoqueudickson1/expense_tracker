b783cc2a586bbb83e4ff57456eaee66b8c658e60
Mardoqueu Dickson <mardoqueudrums1999@gmail.com>
2023-04-02 22:33:09 +0100

    Primeiro dash

---
+++ data/data.js
@@ -0,0 +1,112 @@
+export const data = [
+  {
+    id: 1,
+    name: {
+      first: 'John',
+      last: 'Pedro',
+    },
+    total: 2795.95,
+    status: 'Esperando',
+    method: '10/3/2023',
+    date: 'Há 15 minutos',
+  },
+  {
+    id: 2,
+    name: {
+      first: 'Adão',
+      last: 'Marcos',
+    },
+    total: 1195.95,
+    status: 'Processando',
+    method: '10/3/2023',
+    date: 'Há 23 minutos',
+  },
+  {
+    id: 3,
+    name: {
+      first: 'Sarah',
+      last: 'Pedro',
+    },
+    total: 495.85,
+    status: 'Completado',
+    method: '31/4/223',
+    date: '1 Horas',
+  },
+  {
+    id: 4,
+    name: {
+      first: 'Josepha',
+      last: 'Paula',
+    },
+    total: 150.45,
+    status: 'Processando',
+    method: 'MasterCard',
+    date: '1 Horas',
+  },
+  {
+    id: 5,
+    name: {
+      first: 'Steve',
+      last: 'Harding',
+    },
+    total: 175.25,
+    status: 'Esperando',
+    method: '10/3/2023',
+    date: '2 Horas',
+  },
+  {
+    id: 6,
+    name: {
+      first: 'Laura',
+      last: 'Croft',
+    },
+    total: 1295.75,
+    status: 'Completado',
+    method: '9/11/2023',
+    date: 'Há 8 horas ',
+  },
+  {
+    id: 7,
+    name: {
+      first: 'Michael',
+      last: 'Jones',
+    },
+    total: 89.95,
+    status: 'Completado',
+    method: 'MasterCard',
+    date: 'Há 3 horas',
+  },
+  {
+    id: 8,
+    name: {
+      first: 'Paulo',
+      last: 'Bob',
+    },
+    total: 689.45,
+    status: 'Completado',
+    method: '31/4/223',
+    date: 'Há 7 horas',
+  },
+  {
+    id: 9,
+    name: {
+      first: 'Haley',
+      last: 'Pendente',
+    },
+    total: 14.99,
+    status: 'Completado',
+    method: '10/3/2023',
+    date: 'Há 3 dias',
+  },
+  {
+    id: 10,
+    name: {
+      first: 'Tim',
+      last: 'Thomas',
+    },
+    total: 218.99,
+    status: 'Completado',
+    method: '11/11/2023',
+    date: 'Há 3 dias',
+  },
+];
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,11 @@
 import '@/styles/globals.css'
+import Sidebar from '../components/admin/sidebar'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+  
+  <Sidebar>
+    <Component {...pageProps} />
+  </Sidebar>
+  )
 }
