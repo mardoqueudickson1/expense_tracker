27b7c35f1e6aeed79e5a45a3c079fda63be4eb11
mardoqueudickson1 <mardoqueudrums1999.com>
2023-06-22 19:27:37 +0100

    fix deploy

--- services/axios.js
+++ services/axios.js
@@ -2,13 +2,13 @@ import axios from 'axios';
 import dotenv from 'dotenv';
 dotenv.config();
 
-const baseURL =
-  process.env.REACT_ENV === 'production'
-    ? 'https://potterhouseapi.onrender.com'
-    : 'http://localhost:3001';
+// const baseURL =
+//   process.env.REACT_ENV === 'production'
+//     ? 'https://potterhouseapi.onrender.com'
+//     : 'http://localhost:3001';
 
 const api = axios.create({
-  baseURL: baseURL,
+  baseURL: 'https://potterhouseapi.onrender.com',
 });
 
 export default api;
